ed470ca8c4f837e19ae4dcb86659913820139919
underengineering <san4a852b@gmail.com>
2023-10-24 08:33:06 +0300

    fix: Improve NavBar style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,27 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import useSWR from "swr";
 
 import { Api } from "@/lib/api";
-import { Chat, PeopleAlt } from "@mui/icons-material";
+import { Chat, PeopleAlt, Settings } from "@mui/icons-material";
 
-import Avatar from "./Avatar";
+import Avatar, { AvatarSkeleton } from "./Avatar";
 
-const NavBar = async () => {
-    const me = await Api.Users.me();
+const NavBar = () => {
+    const { data: me } = useSWR("/me", Api.Users.me);
+
+    const pathName = usePathname();
+    const routes = [
+        { text: "Chat", href: "/", icon: <Chat /> },
+        { text: "Friends", href: "/friends", icon: <PeopleAlt /> },
+        { text: "Settings", href: "/settings", icon: <Settings /> },
+    ];
 
     return (
-        <div className="p-2 flex flex-col rounded shadow-md bg-secondary-bg w-48 h-full">
-            {me.isOk ? (
+        <div className="px-2 py-4 flex flex-col rounded shadow-md bg-secondary-bg w-48 h-full">
+            {me?.isOk ? (
                 <div className="flex flex-col items-center">
                     <Avatar hash={me.data.avatar} />
                     <span>
@@ -18,16 +29,24 @@ const NavBar = async () => {
                     </span>
                 </div>
             ) : (
-                <></>
+                <div className="flex flex-col items-center animate-pulse">
+                    <AvatarSkeleton />
+                    <div className="my-1 px-16 rounded-full bg-skeleton whitespace-nowrap overflow-ellipsis w-1/4 h-4"></div>
+                </div>
             )}
-            <nav className="">
+            <nav className="py-4">
                 <ol className="p-2 flex flex-col gap-2">
-                    <Link className="" href="/">
-                        <Chat /> Chat
-                    </Link>
-                    <Link href="/friends">
-                        <PeopleAlt /> Friends
-                    </Link>
+                    {routes.map((route, index) => (
+                        <Link
+                            className={`transition-colors hover:text-primary active:text-secondary ${
+                                route.href === pathName ? "text-primary" : ""
+                            }`}
+                            href={route.href}
+                            key={index}
+                        >
+                            {route.icon} {route.text}
+                        </Link>
+                    ))}
                 </ol>
             </nav>
         </div>
